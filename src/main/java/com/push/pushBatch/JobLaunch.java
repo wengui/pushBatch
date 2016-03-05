1c85958455e9b2a2f7ecd6542cc4a0d6f561c32d
yuting0787 <626808708@qq.com>
2016-03-05 23:34:39 +0800

    做成

---
+++ src/main/java/com/push/pushBatch/JobLaunch.java
@@ -0,0 +1,32 @@
+package com.push.pushBatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.push.bean.gen.UserInfo;
+import com.push.dao.readdao.UserInfoMapper;
+
+public class JobLaunch {
+
+	public static void main(String[] args) {
+        try {
+            ApplicationContext context = new ClassPathXmlApplicationContext("classpath:/config/springBatch.xml");
+ 
+            JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
+            Job job = (Job) context.getBean("helloWorldJob"); 
+ 
+            // JobLauncher可以用来启动Job
+            JobExecution result = jobLauncher.run(job, new JobParameters());
+             
+            // 处理结束，控制台打印处理结果 
+            System.out.println(result.toString());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
