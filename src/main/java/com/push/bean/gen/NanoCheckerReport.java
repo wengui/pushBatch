b0aa83d94eb96b7bf5f12687732960847be17af9
zmj <zmj@zmj-PC>
2016-03-08 18:00:25 +0800

    通知详情

---
+++ src/main/java/com/push/bean/gen/NanoCheckerReport.java
@@ -0,0 +1,73 @@
+package com.push.bean.gen;
+
+
+public class NanoCheckerReport {
+  
+	//病人名字
+    private String PatientName;
+ 
+	//病人年龄
+    private String Age;
+    //项目英文名字
+    private String ItemId;
+    //项目中文名字
+    private String ItemName;
+    //结果值
+    private Integer Value;
+
+    //最小值
+    private Integer Min;
+    //最大值
+    private Integer Max;
+    //参考值
+    private Integer Reference;
+
+    public String getPatientName() {
+ 		return PatientName;
+ 	}
+ 	public void setPatientName(String patientName) {
+ 		PatientName = patientName;
+ 	}
+ 	public String getAge() {
+ 		return Age;
+ 	}
+ 	public void setAge(String age) {
+ 		Age = age;
+ 	}
+ 	public String getItemId() {
+ 		return ItemId;
+ 	}
+ 	public void setItemId(String itemId) {
+ 		ItemId = itemId;
+ 	}
+ 	public String getItemName() {
+ 		return ItemName;
+ 	}
+ 	public void setItemName(String itemName) {
+ 		ItemName = itemName;
+ 	}
+ 	public Integer getValue() {
+ 		return Value;
+ 	}
+ 	public void setValue(Integer value) {
+ 		Value = value;
+ 	}
+ 	public Integer getMin() {
+ 		return Min;
+ 	}
+ 	public void setMin(Integer min) {
+ 		Min = min;
+ 	}
+ 	public Integer getMax() {
+ 		return Max;
+ 	}
+ 	public void setMax(Integer max) {
+ 		Max = max;
+ 	}
+ 	public Integer getReference() {
+ 		return Reference;
+ 	}
+ 	public void setReference(Integer reference) {
+ 		Reference = reference;
+ 	}
+}
--- src/main/java/com/push/dao/readdao/NanoCheckerRefReadMapper.java
+++ src/main/java/com/push/dao/readdao/NanoCheckerRefReadMapper.java
@@ -1,8 +1,16 @@
 package com.push.dao.readdao;
 
+import java.util.Date;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.push.bean.gen.NanoCheckerRef;
+import com.push.bean.gen.NanoCheckerReport;
 
 public interface NanoCheckerRefReadMapper {
 
     NanoCheckerRef selectByPrimaryKey(Integer id);
+    
+    List<NanoCheckerReport> selectByPatientId(@Param("pname")String pname ,@Param("ptime")Date ptime);
 }
