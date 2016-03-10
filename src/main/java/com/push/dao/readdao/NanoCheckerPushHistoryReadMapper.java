2a775e5748c01fb7460d8208fa5ce71fb53eae68
zmj <zmj@zmj-PC>
2016-03-10 13:53:57 +0800

    历史通知一览

--- src/main/java/com/push/dao/readdao/NanoCheckerPushHistoryReadMapper.java
+++ src/main/java/com/push/dao/readdao/NanoCheckerPushHistoryReadMapper.java
@@ -1,8 +1,11 @@
 package com.push.dao.readdao;
 
+import java.util.List;
+
 import com.push.bean.gen.NanoCheckerPushHistory;
 
 public interface NanoCheckerPushHistoryReadMapper {
 	
     NanoCheckerPushHistory selectByPrimaryKey(Integer id);
+    List<NanoCheckerPushHistory> selectAll();
 }
